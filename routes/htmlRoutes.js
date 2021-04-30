89018cf77e72a2b05ee852f2463d1e2516b51de9
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-04-30 09:09:16 -0600

    db + gitignore

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const path = require("path");
 // routing
 module.exports = (app) => {
 // handles when users "visits" a page...the user is shown an HTML page of content
-  app.get("/notes", (req, res) => {index
+  app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
   // If no matching route is found default to index
